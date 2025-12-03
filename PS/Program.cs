90ae1ebd96a93d8afcac4d57665f49bfd64d3858
Ahmed-Elkemety <ahmedelkemety@gmail.com>
2025-12-03 22:27:24 +0200

    w

--- PS/Program.cs
+++ PS/Program.cs
@@ -9,7 +9,7 @@ class Program
 
     public static void Main(string[] args)
     {
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         IEnumerable<int> evenNumbers = numbers.Is_Even( N => N % 2 == 0);
         
         foreach (var num in evenNumbers)
